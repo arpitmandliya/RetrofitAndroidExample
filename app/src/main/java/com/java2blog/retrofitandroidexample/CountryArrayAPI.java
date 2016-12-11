8393dcfe790dac580d4bd58b9a1ecf806472ae1c
apple <apple@apples-MacBook-Air.local>
2016-12-11 23:28:45 +0530

    Initial commit

---
+++ app/src/main/java/com/java2blog/retrofitandroidexample/CountryArrayAPI.java
@@ -0,0 +1,15 @@
+package com.java2blog.retrofitandroidexample;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.http.GET;
+
+public interface CountryArrayAPI {
+
+    /* @GET url is after Base_URL.
+       We are going to get List of country as response.
+    */
+    @GET("/arpitmandliya/AndroidRestJSONExample/master/countries.json")
+    public Call<List<Country>> getCountries();
+}
